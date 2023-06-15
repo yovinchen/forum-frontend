560d986995763b4e64df0e176fe3d081a5ca4a3b
yovinchen <gzh298255@gmail.com>
2023-06-15 17:58:57 +0800

    init

---
+++ src/components/Layout/Footer.vue
@@ -0,0 +1,57 @@
+<template>
+  <footer class="footer has-text-grey-light has-background-grey-darker">
+    <div class="container">
+      <div class="">
+        <span>简洁、实用、美观</span>
+
+        <span style="float: right">
+          <a href="/?lang=zh_CN">中文</a> |
+          <a href="/?lang=en_US">English</a>
+        </span>
+      </div>
+
+      <div>
+        <span>{{ title }} ALL RIGHTS RESERVED</span>
+        <div style="float: right">
+          <template>
+            <b-taglist attached>
+              <b-tag size="is-normal" type="is-dark">Design</b-tag>
+              <b-tag size="is-normal" type="is-info">{{ author }}</b-tag>
+            </b-taglist>
+          </template>
+        </div>
+      </div>
+    </div>
+    <back-top></back-top>
+  </footer>
+</template>
+
+<script>
+import BackTop from "@/components/Backtop/BackTop";
+
+export default {
+  name: "Footer",
+  components: {
+    BackTop
+  },
+  data() {
+    return {
+      title: "© " + new Date().getFullYear() + ' yovinchen',
+      author: 'yovinchen',
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+footer {
+  margin-top: 120px;
+  height: 150px;
+}
+
+footer a {
+  color: #bfbfbf;
+}
+
+</style>
